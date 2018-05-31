f4c1ad898b3494ae9dcfc6f73c674a715deda957
Adrià Fontcuberta <adria@calidae.com>
2018-05-31 07:49:59 +0200

    Add fancy border in header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="Layout Layout--default">
     <header class="Layout__header u-padding-bottom-large">
-      <div class="o-wrapper u-padding u-margin-bottom-large">
-        <nuxt-link to="/" class="u-text-light u-text-h3">
-          <img src="~assets/logo.png" alt="UX Stack" width="200" class="u-margin-bottom">
+      <div class="o-wrapper u-flex u-flex-items-center u-padding u-margin-bottom-large">
+        <nuxt-link to="/" class="Logo u-text-light">
+          <span class="u-text-special-font">UX</span>
+          <span class="u-text-normal">stack</span>
         </nuxt-link>
-        <div class="u-text-h5">
+        <div class="Header__tagline u-text-branding-primary-muted u-text-right u-margin-left-auto u-text-special-font u-text-h4">
           UX Stack is a curated list of cool resources and tools for developers, UX researchers, designers and project managers.
         </div>
       </div>
@@ -14,12 +15,16 @@
         <polygon fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
       </svg>
     </header>
-    <nuxt class="o-wrapper u-margin-bottom-huge" />
-    <footer class="Layout__footer u-padding">
-      <div class="o-wrapper u-flex">
-        Made with 💚 by&nbsp;<FancyLink href="https://twitter.com/afontcu_" title="Twitter">@afontcu_</FancyLink>
+    <nuxt class="o-wrapper u-margin-bottom-huge u-padding-bottom-huge" />
+    <footer class="Footer Layout__footer">
+      <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon fill="#444" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+        <polygon fill="#444" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+      </svg>
+      <div class="Footer__wrapper o-wrapper u-flex u-padding-vertical-large">
+        Made with 💚 by&nbsp;<FancyLink target="_blank" href="https://twitter.com/afontcu_" title="Twitter">@afontcu_</FancyLink>
         <div class="u-margin-left-auto">
-          You can report bugs on <FancyLink href="https://github.com/afontcu/uxstack" title="Github">Github &rarr;</FancyLink>
+          Feel free to report issues or ideas on <FancyLink target="_blank" href="https://github.com/afontcu/uxstack" title="Github">Github &rarr;</FancyLink>
         </div>
       </div>
     </footer>
@@ -58,6 +63,7 @@ html, body {
     "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #2f2f2f;
 }
 
 .Layout__header {
@@ -75,7 +81,26 @@ html, body {
 }
 
 .Layout__footer {
-  color: #444;
-  background-color: #f1f1f2;
+  position: relative;
+  color: #f1f1f2;
+  background-color: #444;
+
+  .Layout__svg {
+    bottom: inherit;
+    top: -100px;
+  }
+}
+
+.Footer__wrapper {
+
+  background-color: #444;
+}
+
+.Header__tagline {
+  max-width: 490px;
+}
+
+.Logo {
+  font-size: 4rem;
 }
 </style>
