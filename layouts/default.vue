5deec668d75b3ed44f03a34091ac0aa3bd72c05c
Adrià Fontcuberta <adria@calidae.com>
2018-05-31 07:25:45 +0200

    Add fancy header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="Layout Layout--default">
-    <header class="Layout__header">
-      <div class="o-wrapper u-padding u-margin-bottom">
+    <header class="Layout__header u-padding-bottom-large">
+      <div class="o-wrapper u-padding u-margin-bottom-large">
         <nuxt-link to="/" class="u-text-light u-text-h3">
           <img src="~assets/logo.png" alt="UX Stack" width="200" class="u-margin-bottom">
         </nuxt-link>
         <div class="u-text-h5">
-          UX Stack is a curated list of resources and tools for developers, UX researchers, designers and project managers.
+          UX Stack is a curated list of cool resources and tools for developers, UX researchers, designers and project managers.
         </div>
       </div>
+      <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+        <polygon fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+      </svg>
     </header>
     <nuxt class="o-wrapper u-margin-bottom-huge" />
     <footer class="Layout__footer u-padding">
@@ -57,8 +61,17 @@ html, body {
 }
 
 .Layout__header {
+  position: relative;
+
   background-color: map-get($global-colors, 'branding-primary');
   color: white;
+}
+
+.Layout__svg {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
 }
 
 .Layout__footer {
