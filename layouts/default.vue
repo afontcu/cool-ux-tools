4c42ecc5aeecdb0f9beb608f3366a3344c0789c4
Adrià Fontcuberta <adria@calidae.com>
2018-06-01 09:13:08 +0200

    Add cool svg pattern header background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="Layout Layout--default">
     <header class="Layout__header u-padding-bottom-large">
-      <div class="o-wrapper u-flex u-flex-items-center u-padding u-margin-bottom-large">
-        <nuxt-link to="/" class="Logo u-text-branding-primary u-flex u-flex-items-baseline u-text-light">
-          <span class="Logo__small u-text-special-font">cool</span>
-          <span class="u-text-hairline u-padding-horizontal-tiny">UX</span>
-          <span class="Logo__small u-text-special-font">tools</span>
-        </nuxt-link>
-        <div class="Header__tagline u-text-branding-primary u-text-right u-margin-left-auto u-text-special-font u-text-h4">
-          Cool UX Tools is a curated list of (obviously) cool resources and tools for developers, UX researchers, designers and project managers.
-        </div>
+      <nuxt-link to="/" class="Logo u-text-branding-primary u-flex u-flex-center u-text-light">
+        <span class="Logo__small u-text-special-font">cool</span>
+        <span class="u-text-hairline u-padding-horizontal-tiny">UX</span>
+        <span class="Logo__small u-text-special-font u-padding-top-large">tools</span>
+      </nuxt-link>
+      <Triangles class="Layout__triangles u-padding-top-large">
+        <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon fill="white" points="0,76 21,100 33,60 45,100 50,75 55,100 72,70 85,100 95,62 100,80 100,100 0,100" />
+        </svg>
+      </Triangles>
+      <div class="o-wrapper o-wrapper--huge Header__tagline u-margin-top-huge u-padding-top u-text-center u-text-branding-primary u-text-special-font u-text-h2">
+        Cool UX Tools is a curated list of (obviously) cool resources and tools for developers, UX researchers, designers and project managers.
       </div>
     </header>
-    <nuxt class="o-wrapper u-margin-bottom-huge u-padding-bottom-huge" />
+    <nuxt class="Layout__main o-wrapper u-margin-bottom-huge u-padding-bottom-huge" />
     <footer class="Footer Layout__footer">
-      <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+      <!-- <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
         <polygon fill="#444" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
         <polygon fill="#444" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
-      </svg>
+      </svg> -->
       <div class="Footer__wrapper o-wrapper u-flex u-padding-vertical-large">
         <div>
           Made with 💚 by&nbsp;<FancyLink target="_blank" href="https://twitter.com/afontcu_" title="Twitter">@afontcu_</FancyLink>
@@ -32,10 +35,12 @@
 
 <script>
 import FancyLink from '@/components/FancyLink'
+import Triangles from '@/components/Triangles'
 
 export default {
   components: {
     FancyLink,
+    Triangles,
   },
 }
 </script>
@@ -66,17 +71,12 @@ html, body {
 }
 
 .Layout__header {
-  position: relative;
-
-  // background-color: map-get($global-colors, 'branding-primary');
-  color: white;
-
+  overflow: hidden;
 }
 
 .Layout__svg {
   position: absolute;
   bottom: 0;
-  width: 100%;
   height: 100px;
 }
 
@@ -84,28 +84,27 @@ html, body {
   position: relative;
   color: #f1f1f2;
   background-color: #444;
-
-  .Layout__svg {
-    bottom: inherit;
-    top: -90px;
-  }
 }
 
 .Footer__wrapper {
-
   background-color: #444;
 }
 
-.Header__tagline {
-  max-width: 490px;
-}
-
 .Logo {
-  font-size: 4rem;
+  position: relative;
+  z-index: 1;
+
+  color: white;
+  font-size: 5rem;
   transform: rotate3d(0, 0, 1, -5deg);
 }
 
 .Logo__small {
-  font-size: 3.5rem;
+  font-size: 4rem;
+}
+
+.Layout__triangles {
+  position: absolute;
+  top: 0;
 }
 </style>
