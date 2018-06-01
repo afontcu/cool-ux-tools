ac47d5a4d57ff0038f5ab18b8e15e5a5598e4bf4
Adrià Fontcuberta <adria@calidae.com>
2018-06-01 17:42:13 +0200

    Remove unused code

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,10 +17,6 @@
     </header>
     <nuxt class="Layout__main o-wrapper u-margin-bottom-huge u-padding-bottom-huge" />
     <footer class="Footer Layout__footer">
-      <!-- <svg class="Layout__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-        <polygon fill="#444" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
-        <polygon fill="#444" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
-      </svg> -->
       <div class="Footer__wrapper o-wrapper u-flex u-padding-vertical-large">
         <div>
           Made with 💚 by&nbsp;<FancyLink target="_blank" href="https://twitter.com/afontcu_" title="Twitter">@afontcu_</FancyLink>
