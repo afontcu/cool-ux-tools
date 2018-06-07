bb5a878e5c5e4c2cc803225580026e4411df2540
Adrià Fontcuberta <adria@calidae.com>
2018-06-07 19:05:20 +0200

    Add go back link

--- pages/_url.vue
+++ pages/_url.vue
@@ -4,22 +4,25 @@
       <h1 class="Category__title u-margin-top-large u-text-center u-text-special-font">{{ category.name }} cool tools</h1>
       <h2 v-if="category.description" class="u-margin-bottom-huge u-text-center u-text-h3">{{ category.description }}</h2>
     </div>
-    <div class="Category__resources-list">
+    <div class="Category__resources-list u-margin-bottom-large">
       <cool-resource v-for="resource in filteredResources"
                      :key="resource.uri"
                      v-bind="resource"
       />
     </div>
+    <FancyLink href="/" title="Go back to Cool UX Tools homepage">&larr; Go Back</FancyLink>
   </div>
 </template>
 
 <script>
 import CoolResource from '@/components/CoolResource'
+import FancyLink from '@/components/FancyLink'
 import { resources } from '~/data/data.json'
 
 export default {
   components: {
     CoolResource,
+    FancyLink,
   },
   asyncData ({ route, error }) {
     const { categories } = require('~/data/data.json')
