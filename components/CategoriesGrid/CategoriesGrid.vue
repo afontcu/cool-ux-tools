dfbcd97fb2a3ef10d93c77d1b530a9d0e680826f
Adrià Fontcuberta <adria@calidae.com>
2018-06-12 08:43:40 +0200

    Improve header visualization on mobile and tablet

--- components/CategoriesGrid/CategoriesGrid.vue
+++ components/CategoriesGrid/CategoriesGrid.vue
@@ -42,36 +42,18 @@ export default {
   position: relative;
   top: 0;
 
-  transition: all ease 0.1s;
+  transition: all ease 0.2s;
   font-size: calc(1rem + 0.5vw);
   background-color: $color-branding-primary;
   border-radius: $global-radius;
   box-shadow: 0 6px $color-branding-primary-accent;
   color: white;
 
-  &::after {
-    content: "";
-    z-index: -1;
-    position: absolute;
-    width: 100%;
-    height: 0;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.1);
-    transition: all ease $global-transition;
-    border-top-left-radius: $global-radius;
-    border-top-right-radius: $global-radius;
-  }
-
   &:hover {
     z-index: 1;
     top: 2px;
     color: white;
     box-shadow: 0 4px $color-branding-primary-accent;
-
-    &::after {
-      height: 100%;
-    }
   }
 
   &:active {
--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -1,20 +1,16 @@
 <template>
-  <header :style="headerStyle" class="Header">
-    <div class="Logo o-wrapper u-text-branding-primary">
+  <header :style="headerStyle" class="Header u-flex u-flex-items-center">
+    <div class="Header__item Logo o-wrapper u-text-branding-primary">
       <nuxt-link to="/" class="Logo__link u-flex u-padding-top-small">
         <span class="Logo__text Logo__text--cool u-text-special-font">cool</span>
         <span class="Logo__text u-text-hairline u-padding-horizontal-tiny">UX</span>
         <span class="Logo__text Logo__text--tools u-text-special-font">tools</span>
       </nuxt-link>
-      <div class="Tagline u-text-h4">
+      <div class="Tagline">
         A curated list of (obviously) cool resources and tools for developers, UX researchers, designers and project managers.
       </div>
     </div>
-    <triangles :height="height" class="Header__triangles">
-      <svg class="Header__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-        <polygon fill="white" points="0,76 21,100 33,60 45,100 50,75 55,100 72,70 85,100 95,62 100,80 100,100 0,100" />
-      </svg>
-    </triangles>
+    <triangles :height="height" class="Header__triangles"/>
   </header>
 </template>
 
@@ -43,19 +39,14 @@ export default {
 
 <style lang="scss">
 .Header {
-  margin-top: $global-spacing-unit;
-}
-
-@media screen and (min-width: $medium-breakpoint) {
-  .Header {
-    margin-top: $global-spacing-unit-large;
-  }
+  padding: $global-spacing-unit-small 0;
 }
 
 .Tagline {
   color: white;
   text-align: center;
   padding-top: $global-spacing-unit;
+  @include font-size(map-get($global-font-sizes, "h4"));
 }
 
 .Logo {
@@ -68,6 +59,7 @@ export default {
   justify-content: center;
   margin-top: $global-spacing-unit-small;
   margin-left: $global-spacing-unit;
+  margin-right: $global-spacing-unit;
 }
 
 .Logo__text {
@@ -115,7 +107,6 @@ export default {
     display: grid;
     grid-template-columns: 1.3fr 1fr;
     grid-gap: $global-spacing-unit;
-    justify-items: start;
     align-items: center;
   }
 
@@ -125,6 +116,20 @@ export default {
 
   .Logo__text--tools, {
     padding-top: $global-spacing-unit-small;
+  }
+}
+
+@media screen and (min-width: $medium-breakpoint) {
+  .Header__item {
+    margin-bottom: $global-spacing-unit-small; // centered, but not so much
+  }
+
+  .Tagline {
+    @include font-size(map-get($global-font-sizes, "h3"));
+  }
+
+  .Logo__link {
+    margin: 0;
   }
 }
 </style>
